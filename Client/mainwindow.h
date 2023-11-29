6764e728d4f3c29c87d208e63b4f7ea906851cf9
kostya2288 <guystruev@gmail.com>
2023-11-29 18:31:03 +0300

    kkewvfd

--- Client/mainwindow.h
+++ Client/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTcpSocket>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +16,21 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_lineEdit_returnPressed();
+
 private:
     Ui::MainWindow *ui;
+    QTcpSocket *socket;
+    QByteArray Data;
+    void sendToServer(QString str);
+    quint16 nextBlockSize;
+
+public slots:
+    void slotReadyRead();
 };
 #endif // MAINWINDOW_H
